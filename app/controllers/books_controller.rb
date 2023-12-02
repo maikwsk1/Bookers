51e7a1240bba4f0165760c10314df5e6a11a9b01
EC2 Default User <ec2-user@ip-172-31-34-91.ap-northeast-1.compute.internal>
2023-12-02 12:23:09 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  
+  def start
+    # Your start action logic here
+  end
+  
+  def new
+    @book = Book.new  
+  end
+  
+  def create
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @books = Book.all  
+  end
+
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
